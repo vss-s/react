73f96d92022994922cf5e035e7c83ee1b79d10c8
honey badger <53346463+vss-s@users.noreply.github.com>
2020-04-23 10:14:50 +0300

    Updates

---
+++ precache-manifest.1734ba3f62df9b8c2b3d65fccc6d01f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74aa1314278ce4390f06486fd1c07da",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "46926e6972eae2dc6535",
+    "url": "/react/static/js/2.6e33d646.chunk.js"
+  },
+  {
+    "revision": "be3eb4cbad5a344e9603583a731df32d",
+    "url": "/react/static/js/2.6e33d646.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9633f3d720b6c00a0dd2",
+    "url": "/react/static/js/main.d59d371e.chunk.js"
+  },
+  {
+    "revision": "8134015fcadbf83c160b",
+    "url": "/react/static/js/runtime-main.a6d1aa66.js"
+  }
+]);
